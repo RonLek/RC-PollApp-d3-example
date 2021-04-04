2ee350c2cedf6a2c21ca3aeb154eee6efce5afcf
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-04-04 11:57:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var fill = d3.scale.category20();
+
+d3.layout.cloud().size([300, 300])
+    .words([
+      "English", "Hindi", "Spanish", "Portuguese", "Mandarin", "Korean", "Japanese",
+      "German"].map(function(d) {
+      return {text: d, size: 10 + Math.random() * 50};
+    }))
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
+
+function draw(words) {
+  d3.select("#word-cloud").append("svg")
+      .attr("width", 300)
+      .attr("height", 300)
+    .append("g")
+      .attr("transform", "translate(150,150)")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
